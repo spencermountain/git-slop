f6ccadf80d1ce3736bb54deff1fd21d5feadf521
spencer kelly <spencermountain@gmail.com>
2018-11-06 13:40:32 -0500

    also try this

--- src/commit.js
+++ src/commit.js
@@ -1,4 +1,5 @@
 var yargs = require('yargs');
+var chalk = require('chalk');
 var exec = require('shelljs').exec;
 var args = yargs
   .usage('gc <msg>')
@@ -9,10 +10,11 @@ let msg = args['_'].join(' ');
 msg = msg.replace(/['"]/g, '');
 msg = msg.trim();
 
-exec();
-
 exec(`git commit -m '${msg}'`, function(code, stdout, stderr) {
-  console.log('Exit code:', code);
-  console.log('Program output:', stdout);
-  console.log('Program stderr:', stderr);
+  if (code !== 0 || stderr) {
+    console.log(stderr);
+    process.exit(1);
+  }
+  console.log('');
+  console.log(chalk.green('   ✅   '));
 });
