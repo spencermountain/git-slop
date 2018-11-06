1c6954e3c6368eefd4ebb73b2f795a70c4e574b4
spencer kelly <spencermountain@gmail.com>
2018-11-06 13:55:13 -0500

    -

--- src/push.js
+++ src/push.js
@@ -1,13 +1,16 @@
 var chalk = require('chalk');
 var shelljs = require('shelljs');
 shelljs.config.silent = true;
+const ora = require('ora');
+
+const spinner = ora(' - ').start();
 
 shelljs.exec(`git push`, function(code, stdout, stderr) {
   if (code !== 0 || stderr) {
     console.log(stderr);
     process.exit(1);
   }
-  console.log(stdout);
   console.log('');
+  spinner.stop();
   console.log(chalk.green('             ✅   '));
 });
