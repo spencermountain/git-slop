a789714872d7474721807757e6fe4c6be1378188
spencer kelly <spencermountain@gmail.com>
2018-11-06 13:43:32 -0500

    try git push

--- src/push.js
+++ src/push.js
@@ -1,5 +1,18 @@
-exec('some_long_running_process', function(code, stdout, stderr) {
-  console.log('Exit code:', code);
-  console.log('Program output:', stdout);
-  console.log('Program stderr:', stderr);
+var yargs = require('yargs');
+var chalk = require('chalk');
+var shelljs = require('shelljs');
+shelljs.config.silent = true;
+
+var args = yargs
+  .usage('gp')
+  .argv;
+
+shelljs.exec(`git push`, function(code, stdout, stderr) {
+  if (code !== 0 || stderr) {
+    console.log(stderr);
+    process.exit(1);
+  }
+  console.log(stdout);
+  console.log('');
+  console.log(chalk.green('             ✅   '));
 });
