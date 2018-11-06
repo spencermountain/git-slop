45b71944ded5fb3d2c6cc16e8ea2f06e866cf508
spencer kelly <spencermountain@gmail.com>
2018-11-06 13:44:33 -0500

    fun times here

--- src/push.js
+++ src/push.js
@@ -1,11 +1,6 @@
-var yargs = require('yargs');
 var chalk = require('chalk');
 var shelljs = require('shelljs');
 shelljs.config.silent = true;
-
-var args = yargs
-  .usage('gp')
-  .argv;
 
 shelljs.exec(`git push`, function(code, stdout, stderr) {
   if (code !== 0 || stderr) {
