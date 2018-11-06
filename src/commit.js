e8bc3b5b09bcb313dec62ad997222f0b5eec6e2f
spencer kelly <spencermountain@gmail.com>
2018-11-06 13:42:35 -0500

    and now theres a checkbox

--- src/commit.js
+++ src/commit.js
@@ -1,6 +1,8 @@
 var yargs = require('yargs');
 var chalk = require('chalk');
-var exec = require('shelljs').exec;
+var shelljs = require('shelljs');
+shelljs.config.silent = true;
+
 var args = yargs
   .usage('gc <msg>')
   .example('gc these pretezels are making me thirsty ')
@@ -10,11 +12,11 @@ let msg = args['_'].join(' ');
 msg = msg.replace(/['"]/g, '');
 msg = msg.trim();
 
-exec(`git commit -m '${msg}'`, function(code, stdout, stderr) {
+shelljs.exec(`git commit -m '${msg}'`, function(code, stdout, stderr) {
   if (code !== 0 || stderr) {
     console.log(stderr);
     process.exit(1);
   }
   console.log('');
-  console.log(chalk.green('   ✅   '));
+  console.log(chalk.green('             ✅   '));
 });
